03a8f41b26725bdd7bd9798431064333c54943cf
Razvan <tzumby@gmail.com>
2017-10-23 10:02:04 -0400

    Started writing tests for ApiAuthentication Plug

--- test/api_authentication_test.exs
+++ test/api_authentication_test.exs
@@ -1,4 +1,13 @@
 defmodule ApiAuthenticationTest do
   use ApiAuthentication.TestCase
-  doctest ApiAuthentication
+
+  alias ApiAuthentication.Test.Repo
+
+  test "assigns secret_id, secret and repo to conn" do
+    conn = ApiAuthentication.call(%Plug.Conn{req_headers: [{"secret", "1234"}, {"secret_id", "123"}]}, Repo)
+
+    assert conn.assigns.secret_id == "123"
+    assert conn.assigns.secret == "1234"
+    assert conn.assigns.repo == Repo
+  end
 end
